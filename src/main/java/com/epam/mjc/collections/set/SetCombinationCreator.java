de624ed0b53d06aa15c77e491b582ebf9983e993
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-08-21 22:44:11 +0300

    add: SetCombinationCreator implementation.

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -1,9 +1,22 @@
 package com.epam.mjc.collections.set;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> result = new TreeSet<>();
+
+        TreeSet<String> intersection = new TreeSet<>(firstSet);
+        intersection.retainAll(secondSet);
+        intersection.removeAll(thirdSet);
+        result.addAll(intersection);
+
+        for (String el : thirdSet) {
+            if (!firstSet.contains(el) && !secondSet.contains(el)) {
+                result.add(el);
+            }
+        }
+        return result;
     }
 }
