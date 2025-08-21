d9a3392378cd6fc4f6fa854ebc54a35b9c5bf05b
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-08-21 22:27:33 +0300

    add: SubsetOfSquaresCreator implementation.

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,14 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        TreeSet<Integer> result = sourceList.stream()
+                .map(x -> x * x)
+                .collect(TreeSet::new, TreeSet::add, TreeSet::addAll);
+
+        return result.subSet(lowerBound, true, upperBound, true);
     }
 }
