02420b673440b02531c38ad1048b9d6502438bee
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-08-21 22:15:04 +0300

    add: HashSetCreator implementation.

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,21 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> result = new HashSet<>();
+        for (Integer num : sourceList) {
+            if (num % 2 == 0) {
+                int current = num;
+
+                while (current % 2 == 0) {
+                    result.add(current);
+                    current /= 2;
+                }
+                result.add(current);
+            } else {
+                result.add(num);
+                result.add(num * 2);
+            }
+        }
+        return result;
     }
 }
